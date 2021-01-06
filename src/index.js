f6f95ce6092eeade6dd17b7a3597ea36ebaf6003
Taylor <tjaroguetech@gmail.com>
2021-01-06 09:30:39 -0500

    Added in a reducer for our eventual users and attached it to the store

--- src/index.js
+++ src/index.js
@@ -3,13 +3,14 @@ import ReactDOM from 'react-dom';
 import { createStore, applyMiddleware, compose } from 'redux'
 import thunk from 'redux-thunk'
 import { Provider } from 'react-redux'
+import userReducer from './reducers/userReducer'
 
 import './index.css';
 import App from './App';
 
 const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION__COMPOSE || compose;
 
-let store = createStore(reducer, composeEnhancers(applyMiddleware(thunk)))
+let store = createStore(userReducer, composeEnhancers(applyMiddleware(thunk)))
 
 ReactDOM.render(
   <Provider store={store}>
---
+++ src/reducers/userReducer.js
@@ -0,0 +1,6 @@
+export default function userReducer(state = {users: []}, action) {
+
+    return state
+
+
+}
