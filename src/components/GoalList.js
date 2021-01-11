323db8569c4c3c8a6c8668e334516400043b987d
Taylor <tjaroguetech@gmail.com>
2021-01-11 17:44:01 -0500

    Built necessary components for rendering individual components for all goals belonging to a user.

--- src/components/GoalList.js
+++ src/components/GoalList.js
@@ -2,11 +2,11 @@ import React from 'react'
 import Goal from './Goal'
 
 
-const GoalList = ({ goals }) => {
+const GoalList = ({ goals }) => (
     <div className="Goal-list">
-        { goals.map(goal => <Goal title={goal.title} description={goal.description} />)}
+        { goals.map(goal => <Goal title={goal.title} description={goal.description} />) }
     </div>
 
-}
+)
 
 export default GoalList
--- src/containers/Profile.js
+++ src/containers/Profile.js
@@ -1,19 +1,23 @@
 import React, { Component } from 'react'
 import { connect } from 'react-redux'
 import {  signOut } from '../actions'
-// import GoalList from '../components/GoalList'
+import GoalList from '../components/GoalList'
 
 class Profile extends Component {
-    state = {
-        goals: []
+    constructor() {
+        super()
+
+        this.state = {
+            goals: []
+        }
     }
 
-    // componentDidMount() {
-    //     fetch(`http://localhost:3000/users/${this.state.userId}/goals`)
-    //         .then(resp => resp.json())
-    //         .then(data => console.log(data))
-    //         // .then(data => this.setState({ goals: data.goals}))
-    // }
+    componentDidMount() {
+        fetch(`http://localhost:3000/users/${this.props.userId}/goals`)
+            .then(resp => resp.json())
+            // .then(data => console.log(data))
+            .then(data => this.setState({ goals: data}))
+    }
 
     signOut = () => {
         this.props.signOut()
@@ -26,7 +30,7 @@ class Profile extends Component {
                 <button onClick={this.signOut} ><a href="/">Sign Out</a></button>
                 <h1>Welcome, {this.props.name}!</h1>
                 <h2>Here's where you're at today:</h2>
-                {/* <GoalList goals={this.state.goals}/> */}
+                <GoalList goals={this.state.goals}/>
             </div>
         )
     }
